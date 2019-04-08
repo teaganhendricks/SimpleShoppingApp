df7b773eda41fbf7deb65c73306c9e967d4ffe68
teagan <teagan.hendricks08@gmail.com>
2019-04-08 15:52:36 +0200

    First Commit

---
+++ SimpleShoppingApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleShoppingApp.WebApiApplication" Language="C#" %>
---
+++ SimpleShoppingApp/Scripts/app.js
@@ -0,0 +1,45 @@
+﻿var currentList = {};
+
+function createShoppingList()
+{
+    currentList.name = $("#shoppingListName").val();
+    currentList.items = new Array();
+
+    showShoppingList();
+
+}
+
+function showShoppingList()
+{
+
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+    $("#newItemName").focus();
+    {
+        if (event.keyCode == 13)
+        {
+            addItem();
+        }
+    };
+}
+
+function addItem()
+{
+
+}
+
+$(document).ready(function () {
+    console.info("I'm ready!")
+
+    $("#shoppingListName").focus();
+    $("#shoppingListName").keyup(function (event)
+    {
+        if (event.keyCode == 13)
+        {
+            createShoppingList();
+        }
+    };
+}
