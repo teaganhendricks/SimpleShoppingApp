75f28c929226737921e3614fc87929a4e21a21c8
teagan <teagan.hendricks08@gmail.com>
2019-04-09 14:55:43 +0200

    second commit

--- SimpleShoppingApp/Scripts/app.js
+++ SimpleShoppingApp/Scripts/app.js
@@ -19,27 +19,64 @@ function showShoppingList()
     $("#shoppingListDiv").show();
     $("#newItemName").focus();
     {
-        if (event.keyCode == 13)
-        {
-            addItem();
-        }
+        $("#newItemName").keyup(function (event) {
+
+            if (event.keyCode == 13)
+            {
+                addItem();
+            }
+        });
     };
 }
 
 function addItem()
 {
+    var newItem = {};
+    newItem.name = $("#newItemName").val();
+    currentList.items.push(newItem);
+    console.log(currentList);
+    showItems();
+    $("#newItemName").val("");
 
+}
+
+function showItems()
+{
+
+    var $list = $("#shoppingListItems").empty();
+
+    for (i = 0; i < currentList.items.length; i++)
+    {
+
+        var currentItem = currentList.items[i];
+        var $li = $("<li>").html(currentItem.name).attr("id" , "item_" + i);
+        var $deleteBtn = $("<button onclick='deleteItem(" + i + ")'>D</button>").appendTo($li);
+        var $checkBtn = $("<button onclick='checkItem(" + i + ")'>C</button>").appendTo($li);
+            
+            $li.appendTo($list);
+
+    }
+
+}
+
+function deleteItem(index)
+{
+    currentList.items.splice(index, 1);
+    showItems();
+}
+
+function checkItem(index)
+{
+    $("#item_" + index).addClass("checked");
 }
 
 $(document).ready(function () {
-    console.info("I'm ready!")
+    //console.info("I'm ready!");
 
     $("#shoppingListName").focus();
-    $("#shoppingListName").keyup(function (event)
-    {
-        if (event.keyCode == 13)
-        {
+    $("#shoppingListName").keyup(function (event) {
+        if (event.keyCode == 13) {
             createShoppingList();
         }
-    };
-}
+    });
+});
